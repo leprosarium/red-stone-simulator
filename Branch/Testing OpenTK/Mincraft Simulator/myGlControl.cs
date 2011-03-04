54ea8d91fdbd61373972cf2223fe0116bff74441
warlockd <warlockd@gmail.com>
2011-03-04 22:50:26 +0000

    Branch off as I figure out keys

---
+++ Branch/Testing OpenTK/Mincraft Simulator/myGlControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
+
+namespace Mincraft_Simulator
+{
+    class myGlControl : OpenTK.GLControl
+    {
+        
+    }
+}
