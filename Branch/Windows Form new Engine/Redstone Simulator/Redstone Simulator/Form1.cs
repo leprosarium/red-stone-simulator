aa7fdfca4b450d1e67ea412a1ec8e098a7d8965b
warlockd <warlockd@gmail.com>
2011-04-25 20:49:42 +0000

    Changing the internal engine

---
+++ Branch/Windows Form new Engine/Redstone Simulator/Redstone Simulator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Redstone_Simulator
+{
+    public partial class Form1 : Form
+    {
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.blockView.ChangeStrip += new BlockView.ChangeStripHandler(blockView_ChangeStrip);
+            this.blockView.select = blockSelect;
+            
+        }
+
+        void  blockView_ChangeStrip(object s, myStatusStripEventArgs e)
+        {
+ 	        if (e.Update.HasFlag(eStatusStripUdate.XYZ))
+            {
+                mainStatusStrip.setCord(e.X, e.Y, e.Z);
+             //   this.cCord.Text = e.X + "x" + e.Y + "y" + e.Z + "z";
+            //    this.cCord.Invalidate();
+            }
+        }
+
+    
+
+        void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            this.MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+            
+            this.blockView.select = blockSelect;
+            if (e.Delta > 0 | e.Delta < 0)
+                blockSelect.moveSelect(e.Delta > 0 ? 1 : -1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blockSelect.Left = (this.Width - blockSelect.Width) / 2;
+            if (blockSelect.Left < 0) blockSelect.Left = 0;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0 | e.Delta < 0)
+                blockSelect.moveSelect(e.Delta);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void blockView1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            blockSelect.Left = (this.Width - blockSelect.Width) / 2;
+            if (blockSelect.Left < 0) blockSelect.Left = 0;
+        }
+    }
+}
---
+++ Branch/Windows Form new Engine/Redstone Simulator/Redstone Simulator/myStatusStrip.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Redstone_Simulator
+{
+    
+
+    public partial class BlockStatusStrip : StatusStrip
+    {
+        int x=0;
+        int y=0;
+        int z=0;
+        int wires, torches,redstone = 0;
+        int layer = 1;
+        ToolStripLabel cLayer;
+        ToolStripLabel cCord;
+        ToolStripLabel cTorches;
+        ToolStripLabel cWires;
+        ToolStripLabel cRedstone;
+        
+        
+        
+        //public event EventHandler<myStatusStripEventArgs> StatusStripEvent;
+
+        
+        private void CreateStrip()
+        {
+            this.SuspendLayout();
+            this.LayoutStyle = ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.cLayer = new ToolStripLabel();
+            this.cLayer.ForeColor = Color.DarkBlue;
+            this.cLayer.Name = "cLayer";
+            this.cLayer.Text = "Layer 1";
+            this.cLayer.Alignment = ToolStripItemAlignment.Left;
+            this.Items.Add(cLayer);
+
+            this.Items.Add(new ToolStripSeparator());
+
+            this.cCord = new ToolStripLabel();
+            this.cCord.ForeColor = Color.Black;
+            this.cCord.Name = "cCord";
+            this.cCord.Text = "0x0y0z";
+            this.cCord.Alignment = ToolStripItemAlignment.Left;
+            this.Items.Add(cCord);
+
+            this.Items.Add(new ToolStripSeparator());
+
+            this.cRedstone = new ToolStripLabel();
+            this.cRedstone.ForeColor = Color.Black;
+            this.cRedstone.Image = Redstone_Simulator.Properties.Resources.Ore16;
+            this.cRedstone.Name = "cRedstone";
+            this.cRedstone.Text = "0";
+            this.cRedstone.Alignment = ToolStripItemAlignment.Right;
+            this.Items.Add(cRedstone);
+
+            this.Items.Add(new ToolStripSeparator() { Alignment = ToolStripItemAlignment.Right });
+
+            this.cTorches = new ToolStripLabel();
+            this.cTorches.ForeColor = Color.Black;
+            this.cTorches.Image = Redstone_Simulator.Properties.Resources.Torch16;
+            this.cTorches.ImageAlign = ContentAlignment.MiddleLeft;
+            this.cTorches.Name = "cTorches";
+            this.cTorches.Text = "0";
+            this.cTorches.Alignment = ToolStripItemAlignment.Right;
+            this.Items.Add(cTorches);
+
+            this.Items.Add(new ToolStripSeparator() { Alignment = ToolStripItemAlignment.Right });
+
+            this.cWires = new ToolStripLabel();
+            this.cWires.ForeColor = Color.Black;
+            this.cWires.Image = Redstone_Simulator.Properties.Resources.Redstone16;
+            this.cWires.Name = "cWires";
+            this.cWires.Text = "0";
+            this.cWires.Alignment = ToolStripItemAlignment.Right;
+            this.Items.Add(cWires);
+
+            this.Items.Add(new ToolStripSeparator() { Alignment = ToolStripItemAlignment.Right });
+
+            this.ResumeLayout(false);
+
+        }
+        public BlockStatusStrip()
+        {
+            this.BackColor = Color.White;
+            CreateStrip();
+            ChangeText();
+        }
+
+        void ChangeText()
+        {
+            cLayer.Text = String.Format("Layer {0,3:d}" , layer);
+            cTorches.Text = String.Format("{0,3:d}", torches);
+            cWires.Text = String.Format("{0,3:d}", wires);
+            cRedstone.Text = String.Format("{0,3:d}", redstone);
+            cCord.Text = x.ToString("d") + "x" +
+                y.ToString("d") + "y" +
+                z.ToString("d") + "z";
+            this.Invalidate(true);
+        }
+
+        public void setCord(int X, int Y, int Z)
+        {
+            x = X; y = Y; z = Z;
+            ChangeText();
+        }
+        public void setWire(int Wires)
+        {
+            wires = Wires;
+            ChangeText();
+        }
+        public void setRedstone(int Redstone)
+        {
+            redstone = Redstone;
+            ChangeText();
+        }
+        public void setTorches(int Torches)
+        {
+            torches = Torches;
+            ChangeText();
+        }
+            
+        public void setLayer(int Layer)
+        {
+            layer = Layer;
+            ChangeText();
+        }
+
+
+   
+    }
+        [Flags]
+        public enum eStatusStripUdate
+        {
+            XYZ = 1,
+            Torches = 2,
+            RedStone = 4,
+            Wires = 8,
+            Layer = 10,
+            Hide = 12
+        }
+    
+        public class myStatusStripEventArgs : EventArgs
+        {
+            public int X { get;  set; }
+            public int Y  { get;  set; }
+            public int Z  { get;  set; }
+            public int Torches  { get;  set; }
+            public int Redstone  { get;  set; }
+            public int Wires  { get;  set; }
+            public int Layer  { get;  set; }
+
+            public eStatusStripUdate Update { get; internal set; }
+
+            public myStatusStripEventArgs(eStatusStripUdate t) { Update = t; X = Y = Z = Torches = Redstone = Wires = Layer = 0; }
+
+
+        }
+        
+}
