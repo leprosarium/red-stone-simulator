fc430844ef4c74869b7a51c08535164f0489236a
bouma.john <bouma.john@gmail.com>
2011-05-16 14:35:11 +0000

    Added form to allow addition of multiple lines to the grid.

---
+++ Branch/Windows Form new Engine/Redstone Simulator/Redstone Simulator/frmAddRowsCols.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Redstone_Simulator
+{
+    partial class frmAddRowsCols
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTop = new System.Windows.Forms.Label();
+            this.lblBottom = new System.Windows.Forms.Label();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tbTop = new System.Windows.Forms.TextBox();
+            this.tbRight = new System.Windows.Forms.TextBox();
+            this.tbLeft = new System.Windows.Forms.TextBox();
+            this.tbBottom = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTop
+            // 
+            this.lblTop.AutoSize = true;
+            this.lblTop.Location = new System.Drawing.Point(9, 9);
+            this.lblTop.Name = "lblTop";
+            this.lblTop.Size = new System.Drawing.Size(26, 13);
+            this.lblTop.TabIndex = 0;
+            this.lblTop.Text = "Top";
+            // 
+            // lblBottom
+            // 
+            this.lblBottom.AutoSize = true;
+            this.lblBottom.Location = new System.Drawing.Point(9, 35);
+            this.lblBottom.Name = "lblBottom";
+            this.lblBottom.Size = new System.Drawing.Size(40, 13);
+            this.lblBottom.TabIndex = 1;
+            this.lblBottom.Text = "Bottom";
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Location = new System.Drawing.Point(9, 57);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(25, 13);
+            this.lblLeft.TabIndex = 2;
+            this.lblLeft.Text = "Left";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Location = new System.Drawing.Point(9, 83);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(32, 13);
+            this.lblRight.TabIndex = 3;
+            this.lblRight.Text = "Right";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(12, 227);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(197, 227);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // tbTop
+            // 
+            this.tbTop.Location = new System.Drawing.Point(127, 2);
+            this.tbTop.Name = "tbTop";
+            this.tbTop.Size = new System.Drawing.Size(100, 20);
+            this.tbTop.TabIndex = 6;
+            // 
+            // tbRight
+            // 
+            this.tbRight.Location = new System.Drawing.Point(127, 80);
+            this.tbRight.Name = "tbRight";
+            this.tbRight.Size = new System.Drawing.Size(100, 20);
+            this.tbRight.TabIndex = 7;
+            // 
+            // tbLeft
+            // 
+            this.tbLeft.Location = new System.Drawing.Point(127, 54);
+            this.tbLeft.Name = "tbLeft";
+            this.tbLeft.Size = new System.Drawing.Size(100, 20);
+            this.tbLeft.TabIndex = 8;
+            // 
+            // tbBottom
+            // 
+            this.tbBottom.Location = new System.Drawing.Point(127, 28);
+            this.tbBottom.Name = "tbBottom";
+            this.tbBottom.Size = new System.Drawing.Size(100, 20);
+            this.tbBottom.TabIndex = 9;
+            // 
+            // frmAddRowsCols
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tbBottom);
+            this.Controls.Add(this.tbLeft);
+            this.Controls.Add(this.tbRight);
+            this.Controls.Add(this.tbTop);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.lblLeft);
+            this.Controls.Add(this.lblBottom);
+            this.Controls.Add(this.lblTop);
+            this.Name = "frmAddRowsCols";
+            this.Text = "frmAddRowsCols";
+            this.Load += new System.EventHandler(this.frmAddRowsCols_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTop;
+        private System.Windows.Forms.Label lblBottom;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox tbTop;
+        private System.Windows.Forms.TextBox tbRight;
+        private System.Windows.Forms.TextBox tbLeft;
+        private System.Windows.Forms.TextBox tbBottom;
+    }
+}
